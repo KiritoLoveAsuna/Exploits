31f4db2e50d20c0afbae15e310af7f3025233d21
Kirito <38044499+KiritoLoveAsuna@users.noreply.github.com>
2022-01-20 10:36:21 +0800

    Add files via upload

---
+++ CVE-2021-21220/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./"))
+	http.Handle("/", fs)
+
+	log.Println("Listening on :3000... test url http://localhost:3000/exploit.html")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
